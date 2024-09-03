403ed42883e4ec4591c8b42727e5cd91230fcb1f
PraveenkuamrVenkatesan <PraveenkuamrVenkatesan>
2024-09-03 07:06:50 +0000

    saving files

---
+++ Exercise- Data Transformations 2.py
@@ -0,0 +1,133 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC **1. Retrieve all employees who work in the "Finance" department and filter employees with a salary greater than $80,000.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **2. Select only the Name, Age, and City columns and drop the 'Joining Date' column.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **3. Rename the column 'Salary' to 'Annual Income' and change the 'City' column name to 'Location'.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **4. Calculate the average salary of employees in each department and find the total number of employees in each city.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **5. Sort the data by Age in ascending order and Salary in descending order.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **6. Create a new column called 'Years of Experience', assuming everyone joined with zero experience, and add the number of years since their 'Joining Date' and add a column 'Senior Citizen' that marks 'True' for employees aged 40 or above and 'False' for others.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **7. Join employee.csv and department.csv on the Department column to include the manager’s name in the employee data.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **8. Find all employees managed by "John"**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **9. List all departments that do not have an employee.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **10. Convert the 'Review Date' column to a datetime format and calculate the number of days since the last performance review for each employee.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **11. Create a new column Performance Level based on 'Performance Score' where "A" is "High", "B" is "Medium", and "C" is "Low" and convert all 'Performance Score' values to lowercase.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **12. Calculate the average performance score (A=1, B=2, C=3) for each employee and rank employees based on their performance scores and review dates.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **13. In the combined dataset, calculate the cumulative sum of salaries for each department and rank employees within each department based on their 'Annual Income'.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **14. Introduce NaN values in the Age column and fill them with the average age and drop any rows where Performance Score is missing.**
+
+# COMMAND ----------
+
+#Paste your Answer here
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **15. Pivot the performance data to show each employee's score for different review dates and Unpivot the employee data to create a long format table with ID, Attribute, and Value columns.**
+
+# COMMAND ----------
+
+#Paste your Answer here
---
+++ demo.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+a=1
+b=2
+print(a+b)
